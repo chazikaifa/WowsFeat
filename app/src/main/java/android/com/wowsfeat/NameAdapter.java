fd12c0417b0deabfc40187648b52838f36ec2f18
chazikaifa <595056078@qq.com>
2016-09-26 23:30:02 +0800

    这个其实是刚刚做好玩家数据页面的版本
    操作问题导致版本错乱。。。手动Revert到正确版本

--- app/src/main/java/android/com/wowsfeat/NameAdapter.java
+++ app/src/main/java/android/com/wowsfeat/NameAdapter.java
@@ -1,7 +1,6 @@
 package android.com.wowsfeat;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
 import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -37,7 +36,6 @@ public class NameAdapter extends ArrayAdapter<String> {
         playerList = objects;
     }
 
-    @NonNull
     @Override
     public View getView(final int position, View convertView, ViewGroup parent) {
         View view;
